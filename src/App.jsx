86579ec78722dd883b8242f383a94d72882c161c
rajib-faysal <rajib.faysal.201@gmail.com>
2024-08-27 19:49:11 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="text-green-600 text-4xl font-bold ">
+        Removed all bloatware!
+      </h1>
+
+      <button className="mt-9 btn btn-outline btn-accent ">Sign up!</button>
+    </div>
+  );
+}
+
+export default App;
